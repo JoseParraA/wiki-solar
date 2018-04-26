c829937a8d706ffbae85ff5d470ba000b6b42c60
Sam Cochrane <sam.cochrane.88@gmail.com>
2018-04-26 16:42:23 +1000

    sprockets-es6

--- app/assets/javascripts/wiki.js
+++ app/assets/javascripts/wiki.js
@@ -1,33 +1,33 @@
-// $(document).ready(function(){
-// var $results = $("h2").text();
-//
-//
-//   $.ajax({
-//       type: "GET",
-//       url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + $results + "&callback=?",
-//       contentType: "application/json; charset=utf-8",
-//       async: false,
-//       dataType: "json",
-//       // let search =
-//       success: function (data, textStatus, jqXHR) {
-//
-//         var result = $("<h1></h1>").text(data);
-//         $(".system").append(data.parse.text["*"]);
-//
-//           // console.log(data.parse.text["*"]);
-//
-//           // let links = $('a');
-//           // console.log(links);
-//
-//           var paras = $('p').text();
-//           //debugger;
-//         // console.log(paras);
-//           //debugger;
-//           $(".solar-system").append(paras);
-//       },
-//       error: function (errorMessage) {
-//       }
-//   });
-// });
-//
-// // http://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=max&explaintext&exintro&titles=Yahoo|Google&redirects=
+$(document).ready(function(){
+var $results = $("h2").text();
+
+
+  $.ajax({
+      type: "GET",
+      url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=" + $results + "&callback=?",
+      contentType: "application/json; charset=utf-8",
+      async: false,
+      dataType: "json",
+      // let search =
+      success: function (data, textStatus, jqXHR) {
+
+        var result = $("<h1></h1>").text(data);
+        $(".system").append(data.parse.text["*"]);
+
+          // console.log(data.parse.text["*"]);
+
+          // let links = $('a');
+          // console.log(links);
+
+          var paras = $('p').text();
+          //debugger;
+        // console.log(paras);
+          //debugger;
+          $(".solar-system").append(paras);
+      },
+      error: function (errorMessage) {
+      }
+  });
+});
+
+// http://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exlimit=max&explaintext&exintro&titles=Yahoo|Google&redirects=
